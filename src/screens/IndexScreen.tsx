3dd9562e699fd842d658b2601bde2db2591dc8ac
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-07-20 21:04:44 -0700

    add stylings to blog posts

--- src/screens/IndexScreen.tsx
+++ src/screens/IndexScreen.tsx
@@ -7,9 +7,16 @@ import DeleteIcon from 'react-native-vector-icons/FontAwesome';
 const BlogPost = styled.Text`
   color: rgb(38, 38, 38);
   font-family: 'Montserrat-Regular';
-  font-size: 14px;
-  margin-left: 20px;
+  font-size: 18px;
   text-align: left;
+`;
+const BlogView = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding-vertical: 20px;
+  padding-horizontal: 10px;
+  border-top-width: 1px;
+  border-color: gray;
 `;
 
 const IndexScreen = () => {
@@ -23,10 +30,10 @@ const IndexScreen = () => {
         keyExtractor={blogPost => blogPost.title}
         renderItem={({item}) => {
           return (
-            <View>
+            <BlogView>
               <BlogPost>{item.title}</BlogPost>
-              <DeleteIcon name="trash" size={30} color="#900" />
-            </View>
+              <DeleteIcon name="trash" size={24} color="#900" />
+            </BlogView>
           );
         }}
       />
