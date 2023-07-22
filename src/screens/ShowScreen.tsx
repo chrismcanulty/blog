ebc38812a3cb8c27f7c60ec0feaa936f82633ae4
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-07-21 20:02:07 -0700

    pass id parameter to showscreen

--- src/screens/ShowScreen.tsx
+++ src/screens/ShowScreen.tsx
@@ -4,12 +4,21 @@ import {useBlogContext} from '../../context/BlogContext';
 import {NativeStackHeaderProps} from '@react-navigation/native-stack';
 // import styled from 'styled-components/native';
 
-const ShowScreen = ({navigation}: NativeStackHeaderProps) => {
+const ShowScreen = ({
+  navigation,
+  route,
+}: {
+  navigation: NativeStackHeaderProps;
+  route: any;
+}) => {
   const {} = useBlogContext();
+
+  const blogId = route.params.id;
 
   return (
     <View>
       <Text>Show Screen</Text>
+      <Text>Blog Id: {blogId}</Text>
     </View>
   );
 };
