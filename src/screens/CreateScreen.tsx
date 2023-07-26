67890b6ef88500821ae35e198e8135a9030e0f4c
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-07-25 22:56:52 -0700

    created two text input fields in createscreen

--- src/screens/CreateScreen.tsx
+++ src/screens/CreateScreen.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TextInput} from 'react-native';
 // import {useBlogContext} from '../../context/BlogContext';
 // import {NativeStackHeaderProps} from '@react-navigation/native-stack';
 // import styled from 'styled-components/native';
 
-const CreateScreen = () => {
+const CreateScreen = ({navigation}: any) => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
   // const {blogPosts} = useBlogContext();
 
   // const blogId = route.params.id;
@@ -13,7 +15,10 @@ const CreateScreen = () => {
 
   return (
     <View>
-      <Text>Create Screen</Text>
+      <Text>Enter Title:</Text>
+      <TextInput value={title} onChangeText={text => setTitle(text)} />
+      <Text>Enter Content:</Text>
+      <TextInput value={content} onChangeText={text => setContent(text)} />
     </View>
   );
 };
