a2f0c9e50f554b63f4c2e369deae4f322efba1cd
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-07-25 23:04:27 -0700

    add submit button and add stylings

--- src/screens/CreateScreen.tsx
+++ src/screens/CreateScreen.tsx
@@ -1,10 +1,27 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput} from 'react-native';
+import {View, Button} from 'react-native';
 // import {useBlogContext} from '../../context/BlogContext';
 // import {NativeStackHeaderProps} from '@react-navigation/native-stack';
-// import styled from 'styled-components/native';
+import styled from 'styled-components/native';
 
-const CreateScreen = ({navigation}: any) => {
+const InputField = styled.TextInput`
+  border-color: black;
+  border-width: 1;
+  color: rgb(38, 38, 38);
+  font-family: 'Montserrat-Regular';
+  font-size: 18px;
+  margin: 5px;
+  margin-bottom: 15px;
+  padding: 5px;
+`;
+const InputLabel = styled.Text`
+  font-family: 'Montserrat-Regular';
+  font-size: 20px;
+  margin-bottom: 5px;
+  margin-left: 5px;
+`;
+
+const CreateScreen = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   // const {blogPosts} = useBlogContext();
@@ -15,10 +32,11 @@ const CreateScreen = ({navigation}: any) => {
 
   return (
     <View>
-      <Text>Enter Title:</Text>
-      <TextInput value={title} onChangeText={text => setTitle(text)} />
-      <Text>Enter Content:</Text>
-      <TextInput value={content} onChangeText={text => setContent(text)} />
+      <InputLabel>Enter Title:</InputLabel>
+      <InputField value={title} onChangeText={text => setTitle(text)} />
+      <InputLabel>Enter Content:</InputLabel>
+      <InputField value={content} onChangeText={text => setContent(text)} />
+      <Button title="Add Blog Post" />
     </View>
   );
 };
